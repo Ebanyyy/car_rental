aade2e4f9d5be5012f2d9b7224b69f593e1ccc53
Ebanyyy <vaniegabu@gmail.com>
2023-10-23 15:47:40 +0800

    Create car_form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable
+         
+  has_many :cars, dependent: :destroy
 end
