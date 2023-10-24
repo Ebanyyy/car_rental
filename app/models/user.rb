3b5df07f59833027476404dd9a489bbc3bc3a3ab
Ebanyyy <vaniegabu@gmail.com>
2023-10-24 16:32:46 +0800

    Add role in database

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,14 @@ class User < ApplicationRecord
   has_many :cars, dependent: :destroy
   has_many :rentals, dependent: :destroy
   has_many :reviews, dependent: :destroy
+
+  enum :role, { customer: 0, superadmin: 1 }
+
+  def is_superadmin?
+    role == 'superadmin'
+  end
+
+  def is_customer?
+    role == 'customer'
+  end
 end
