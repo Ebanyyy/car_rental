308bd370fde5527d9cadf5d1aa0f2c0e284ce18d
Ebanyyy <vaniegabu@gmail.com>
2023-10-26 22:48:25 +0800

    Add title to inquiry

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_25_060754) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_26_144718) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,6 +61,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_25_060754) do
     t.text "message_box"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
   create_table "rentals", force: :cascade do |t|
