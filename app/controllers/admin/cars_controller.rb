9250ded4d1c89e2a06561987dba7672d85fab161
Ebanyyy <vaniegabu@gmail.com>
2023-11-07 15:15:01 +0800

    add rental dashboard to admin

--- app/controllers/admin/cars_controller.rb
+++ app/controllers/admin/cars_controller.rb
@@ -3,7 +3,12 @@ class Admin::CarsController < BaseController
 	before_action :set_car, only: [:show, :edit, :update, :destroy]
 
 	def index 
-		@cars = Car.all
+		if params[:car_name]
+			@cars = Car.where("car_name LIKE ? OR description LIKE ?", 
+			"%#{params[:car_name]}%", "%#{params[:car_name]}%")
+		else
+			@cars = Car.all
+		end
 	end
 
 	def new
--- app/controllers/customer/inquiries_controller.rb
+++ app/controllers/customer/inquiries_controller.rb
@@ -17,7 +17,7 @@ class Customer::InquiriesController < ApplicationController
 		@inquiry = Inquiry.new(inquiry_params)
 
 		if @inquiry.save
-			redirect_to customer_inquiries_path
+			redirect_to customer_inquiry_path(@inquiry)
 		else
 			render 'new'
 		end
