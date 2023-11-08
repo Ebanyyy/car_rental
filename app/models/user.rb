551df4d71e845de17892f9a2eaf240eba649ec97
bany <baniegabu@gmail.com>
2023-11-08 17:52:55 +0800

    sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable, :confirmable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :cars, dependent: :destroy
