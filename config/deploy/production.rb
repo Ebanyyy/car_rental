b6ec10fa20ac557f42524981667b175a30bc2f04
Ebanyyy <vaniegabu@gmail.com>
2023-11-08 14:19:23 +0800

    installed capistrano 2nd commit

---
+++ config/deploy/production.rb
@@ -0,0 +1,9 @@
+set :stage,           :production
+set :rails_env,       :productiom
+set :branch,          :main
+
+set :application,     'car_rental'
+set :deploy_to,       "/home/#{fetch(:user)}/#{fetch(:application)}"
+set :puma_bind,       "unix://#{shared_path}/sockets/puma.sock"
+
+server '13.212.35.55', user: fetch(:user), roles: [:web, :app, :db], primary: true
